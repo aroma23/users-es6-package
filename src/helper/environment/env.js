9e2014cc0a4648352d54c2cc956737f2e41be5cd
Muthu <muthu@Muthus-MacBook-Air.local>
2025-02-26 12:38:47 -0500

    fixed the issue in getValueFromEnvironment

--- src/helper/environment/env.js
+++ src/helper/environment/env.js
@@ -6,7 +6,7 @@
 */
 
 export const getValueFromEnvironment = (key) => {
-    const holder = (typeof holder != 'undefined' && Cypress.env) ? Cypress.env(key) : process.env[key]
+    const holder = (typeof Cypress != 'undefined' && Cypress.env) ? Cypress.env(key) : process.env[key]
     if (holder === "" || baseUrl === 'undefined') {
         console.error(key + " is not defined in .env or environment variables")
     }
